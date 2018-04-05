29a237efb3736d0e188b43308c5113648735263e
Jose Moreno <jmoreno5@ggc.edu>
2018-04-05 13:39:28 -0400

    adding PHP stuff

---
+++ testingphp/loginform.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!--title>Sign-Up/Login Form</title-->
+    <title>Login Form</title>
+    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
+	
+</head>
+
+<body>
+    <div class="form">
+
+        <div id="login">
+            <h1>Welcome Workout Tracker!</h1>
+
+            <form action="login.php" method="post">
+
+                <div class="field-wrap">
+                    <label>
+                        Email Address<span class="req">*</span>
+                    </label>
+
+                    <input type="email" required autocomplete="off" name="email" />
+                </div>
+
+                <div class="field-wrap">
+                    <label>
+                        Password<span class="req">*</span>
+                    </label>
+
+                    <input type="password" required autocomplete="off" name="password"/>
+                </div>
+
+                <p class="forgot"><a href="signup.html">Need an Account?</a></p>
+
+                <!--button class="button button-block"/>Log In</button-->
+                <!--button class="button button-block" onClick="document.location.href='homepage.html';">Log In</button-->
+                <!--button class="button button-block" onClick="document.location.href='homepage.html';">Log In</button-->
+                <!--button type="submit" class="button button-block" onClick="document.location.href='homepage.html';">Log In</button-->
+
+                <button type="submit" onClick="document.location.href='homepage.php' class="button button-block">Log In</button>
+
+            </form>
+        </div>
+    </div>
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/index.js"></script>
+
+</body>
+</html>
---
+++ testingphp/signup.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+define('DB_NAME', 'webdev');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if(!$link){
+	die('Could not connect: ' . mysql_error());
+}
+	
+$db_selected = mysql_select_db(DB_NAME, $link);
+	
+if (!$db_selected){
+	die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+}
+		
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
+$email = $_POST['emailaddress'];
+$password1 = $_POST['password1'];
+$password2 = $_POST['password2'];
+$gender = $_POST['gender'];
+$height = $_POST['height'];
+$age = $_POST['age'];
+$weight = $_POST['weight'];
+		
+$sql = "INSERT INTO users (fname, lname, email, password, gender, height, age, weight) VALUES ('$fname','$lname', '$email', '$password1', '$gender', '$height', '$age', '$weight')";
+		
+if (!mysql_query($sql)){
+	die('Error:' . mysql_error());
+}
+		
+mysql_close();
+header( 'Location: /webdev/loginform.php' );
+		
+?>
